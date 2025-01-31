a3f8c7b39e5a637de45974ac38235cb7ebd47089
Matt Wicks [SSW] <wicksipedia@users.noreply.github.com>
2025-01-31 12:37:52 +0000

    Update zsh-dotnet-completion.plugin.zsh
    based off https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete#zsh

--- zsh-dotnet-completion.plugin.zsh
+++ zsh-dotnet-completion.plugin.zsh
@@ -1,7 +1,18 @@
+# zsh parameter completion for the dotnet CLI
+
 _dotnet_zsh_complete()
 {
   local completions=("$(dotnet complete "$words")")
 
-  reply=( "${(ps:\n:)completions}" )
+  # If the completion list is empty, just continue with filename selection
+  if [ -z "$completions" ]
+  then
+    _arguments '*::arguments: _normal'
+    return
+  fi
+
+  # This is not a variable assignment, don't remove spaces!
+  _values = "${(ps:\n:)completions}"
 }
-compctl -K _dotnet_zsh_complete dotnet
+
+compdef _dotnet_zsh_complete dotnet
